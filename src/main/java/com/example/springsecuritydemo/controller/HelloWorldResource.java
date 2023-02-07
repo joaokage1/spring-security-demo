f59e505b538b58c917a5a6632daa515808386bd7
joaokage1 <j_vgomes@hotmail.com>
2023-02-07 15:17:02 -0300

    Initial commit

---
+++ src/main/java/com/example/springsecuritydemo/controller/HelloWorldResource.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritydemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldResource {
+
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
---
+++ src/main/java/com/example/springsecuritydemo/model/Todo.java
@@ -0,0 +1,2 @@
+package com.example.springsecuritydemo.model;
+public record Todo (String username, String description) {}
