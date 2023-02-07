aad00403190fa6d49fdf056d4a2b7d21e8558efb
joaokage1 <j_vgomes@hotmail.com>
2023-02-07 15:35:27 -0300

    Using BCrypt

--- src/main/java/com/example/springsecuritydemo/security/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/example/springsecuritydemo/security/BasicAuthSecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -34,12 +35,14 @@ public class BasicAuthSecurityConfiguration {
     public UserDetailsService userDetailsService(DataSource dataSource){
 
         var user = User.withUsername("jogomes")
-                .password("{noop}1234")
+                .password("1234")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
                 .roles("USER")
                 .build();
 
         var admin = User.withUsername("admin")
-                .password("{noop}1234")
+                .password("1234")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
                 .roles("ADMIN")
                 .build();
 
@@ -56,5 +59,10 @@ public class BasicAuthSecurityConfiguration {
                 .setType(EmbeddedDatabaseType.H2)
                 .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
                 .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
